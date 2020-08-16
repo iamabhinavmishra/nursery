3e4ecf6b2dcc0ddc09cf6cde045410ac4aeccf7a
iamabhinavmishra <69203933+iamabhinavmishra@users.noreply.github.com>
2020-08-16 19:13:45 +0530

    update

--- frontend/project/src/core/cart.js
+++ frontend/project/src/core/cart.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import Base from "./Base";
-
+import { createOrder } from "./helper.js/OrderHelper";
 import Card from "./Card";
 import { loadCart } from "./helper.js/cartHelper";
+import { Redirect } from "react-router-dom";
 
 
 const Cart = () => {
@@ -18,7 +19,7 @@ const Cart = () => {
       <div>
         {products.map((product, index) => (
           <Card
-            key={index}
+            id= {index}
             product={product}
             removeFromCart={true}
             addtoCart={false}
@@ -30,13 +31,50 @@ const Cart = () => {
     );
   };
 
-  const loadCheckout = () => {
+  const getAmount = () => {
+    let amount = 0;
+    products.map((p) => {
+      amount = amount + parseInt(p.price);
+    });
+    return amount;
+  };
+
+  let product_names = "";
+            products.forEach(function (item) {
+              product_names += item.name + ", ";
+            });
+  
+            const orderData = {
+              products: product_names,
+              amount: getAmount,
+              transaction_id: "0"
+            };
+            createOrder(orderData)
+              .then((response) => {
+                if (response.error) {
+                  if (response.code == "1") {
+                    console.log("Order Failed!");
+                    
+                      return <Redirect to="/" />;
+                    };
+                  }
+                 else {
+                  if (response.success == true) {
+                    console.log("ORDER PLACED!!");
+                  }
+                }
+              })
+              .catch((error) => {
+                //setInfo({ loading: false, success: false });
+                console.log("Order FAILED", error);
+              });
+  /*const loadCheckout = () => {
     return (
       <div>
         <h1>Checkout</h1>
       </div>
     );
-  };
+  };*/
 
   return (
     <Base title="Cart page" description="Welcome to checkout">
@@ -46,7 +84,19 @@ const Cart = () => {
             <h4>No products</h4>
           )}
         </div>
-        
+        <div>
+        <h1>Checkout</h1>
+        <div>
+      <h3>Your bill is ₹ {getAmount()}</h3>
+      
+    </div>
+      </div>
+      <button
+                //onClick={()}
+                className="btn btn-block btn-success"
+              >
+                Buy Now
+              </button>   
       </div>
     </Base>
   );
